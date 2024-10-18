c3a1a1ddd8baaa3fe3ad23529973e738ea62e532
ayash911 <ya828197@gmail.com>
2024-10-18 14:24:51 +0545

    changesForRender

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template, send_file
+from flask import Flask, request, render_template, send_file, abort
 from pydub import AudioSegment
 import numpy as np
 import tempfile
+import os
 
 app = Flask(__name__)
 
@@ -31,6 +32,7 @@ def generate_morse_audio(morse_code):
             audio += beep + AudioSegment.silent(duration=100)
         else:
             audio += AudioSegment.silent(duration=200)
+
     with tempfile.NamedTemporaryFile(delete=False, suffix='.wav') as tmp_file:
         audio.export(tmp_file.name, format='wav')
         return tmp_file.name
@@ -40,14 +42,18 @@ def home():
     morse_code = ''
     input_text = ''
     audio_file_path = ''
+    
     if request.method == 'POST':
         input_text = request.form['text']
         morse_code = convert_to_morse(input_text)
         audio_file_path = generate_morse_audio(morse_code)
-    return render_template('index.html',input_text = input_text, morse_code=morse_code, audio_file=audio_file_path)
+
+    return render_template('index.html', input_text=input_text, morse_code=morse_code, audio_file=audio_file_path)
 
 @app.route('/play/<path:filename>')
 def play_audio(filename):
+    if not os.path.isfile(filename):
+        return abort(404, description="File not found")
     return send_file(filename, mimetype='audio/wav')
 
 if __name__ == '__main__':
